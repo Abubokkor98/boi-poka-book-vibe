76f26283b52ebfde5e074c0e54a27c898973bfca
Abubokkor98 <mail.abubokkor@gmail.com>
2024-11-03 21:18:38 +0600

    load book details data and show == vs ===

--- src/Components/BookDetail/BookDetail.jsx
+++ src/Components/BookDetail/BookDetail.jsx
@@ -1,12 +1,41 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useLoaderData, useParams } from 'react-router-dom'
 
 export default function BookDetail() {
   const {bookId} = useParams();
-  console.log(bookId);
+  const id = parseInt(bookId);
+  const data = useLoaderData();
+
+//   console.log(typeof bookId, typeof id, typeof data[0].bookId);
+
+const book = data.find(book => book.bookId === id);
+console.log(book);
+
+
+
   return (
-    <div>
-        <h2>Book Detail {bookId}</h2>
+    <div className='grid grid-cols-1 md:grid-cols-2 p-5'>
+        <div>
+            <img src={book.image} alt="" />
+        </div>
+        <div className='space-y-5'>
+            <h1 className="text-5xl">{book.bookName}</h1>
+            <p>By: {book.author}</p>
+            <hr />
+            {book.category}
+            <hr />
+            <p><span className='font-bold'>Review </span>{book.review}</p>
+            <hr />
+            <p>Number of Pages: <span className='font-bold'>{book.totalPages}</span></p>
+            <p>publisher: {book.publisher}</p>
+            <p>Year of Publishing {book.yearOfPublishing}</p>
+            <p>Rating {book.rating}</p>
+            <div className='md:flex gap-5'>
+            <button className='btn btn-outline mr-5'>Read</button>
+            <button className='btn btn-accent'>Wishlist</button>
+            </div>
+        </div>
+        
     </div>
   )
 }
